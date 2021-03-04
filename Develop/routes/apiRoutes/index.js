7d01ad2b2945d1b75026e95036f8c1e8012496b7
Markki Meyer <markkimeyer@gmail.com>
2021-03-03 18:37:40 -0600

    modularized

---
+++ Develop/routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const fs = require("fs");
+
+// const routes = require("../apiRoutes/routes");
+
+// router.use(routes);
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 //express dependencies
-
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 var express = require("express");
 var app = express();
 
@@ -16,28 +17,31 @@ app.use(express.json());
 //set public folder
 app.use(express.static('public'));
 
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 //HTML ROUTES
 
-//GET `/notes` - Should return the `notes.html` file.
 
-module.exports = function (app) {
 
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+    //API ROUTES
+    //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+//     let data = require("/db/db.json")
+//     app.get("/api/notes", function (req, res) {
+//         res.json(data);
+//         console.log(data);
+//     });
 
-    
-    //GET `*` - Should return the `index.html` file
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
+//     //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
-};
+//     app.post("/api/notes", function (req, res) {
+//         data.push(req.body);
+//         res.json(true);
+//     });
 
-//API ROUTES
-//GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
-//POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+//     app.post("")
+
+//  };
 
 //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
