2f44db4a5a40917011730632c55cf87120bfc16d
Markki Meyer <markkimeyer@gmail.com>
2021-03-06 15:40:55 -0600

    still undefined

--- Develop/routes/apiRoutes/apiroutes.js
+++ Develop/routes/apiRoutes/apiroutes.js
@@ -1,36 +1,27 @@
 
 //API ROUTES
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-const data = require("../../db/db.json");
+let notes = require("../../db/db.json");
 const fs = require('fs');
 const path = require("path");
-// const uuid = require("uuid");
+
 
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
-        res.json(data);
+        console.log("successful get");
+        res.json(notes);
+        console.log(notes);
     });
 
-
-
-    //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
     app.post("/api/notes", function (req, res) {
-        const newNote = req.body;
-        //set unique IDs using the UUID module
-        // newNote.id = uuid.v4();
-        data.push(newNote);
-        console.log(data);
-        fs.writeFileSync(
-            path.join(__dirname, '../../db.json'),
-            JSON.stringify(data)
-        );
-        res.json(newNote);
+        let newNote = req.body;
+        console.log("successful API hit");\
         console.log(newNote);
-    });
-
-};
+        });
+    
+    };
 
 
 
--- Develop/server.js
+++ Develop/server.js
@@ -26,6 +26,13 @@ app.use("/", htmlRoutes);
 //HTML ROUTES
 
 
+//listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
+
+
+
 
     //API ROUTES
     //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
@@ -37,10 +44,10 @@ app.use("/", htmlRoutes);
 
 //     //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 
-    app.post("/api/notes", function (req, res) {
-        data.push(req.body);
-        res.json(true);
-    });
+    // app.post("/api/notes", function (req, res) {
+    //     data.push(req.body);
+    //     res.json(true);
+    // });
 
 
 //     app.post("")
@@ -50,7 +57,3 @@ app.use("/", htmlRoutes);
 //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 
 
-//listener
-app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
